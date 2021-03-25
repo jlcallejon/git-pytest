9e023b8d6192f3697db82205cad9b6c6c89cd830
danimateos <dmateos.sanmartin@gmail.com>
2021-03-25 07:05:42 +0100

    Add tests for pyspark
    Add a fixture that provides a SparkSession that can be reused for a whole testing session.

--- paypay.py
+++ paypay.py
@@ -1,3 +1,6 @@
+from pyspark.sql import SparkSession, functions
+
+
 def iseven(n: int) -> bool:
     return n % 2 == 0
 
@@ -15,3 +18,9 @@ def sqrt(n: float, threshold: 1e-3) -> float:
         rel_error = abs(L - W) / L
 
     return L
+
+
+def count_customers(path: str, spark: SparkSession) -> int:
+    df = spark.read.csv(path, header=True, inferSchema=True)
+    result = df.select(functions.countDistinct('msisdn').alias('result'))
+    return result.first()['result']
--- test_paypay.py
+++ test_paypay.py
@@ -1,4 +1,7 @@
+from pyspark.sql import SparkSession
+
 import paypay
+import pytest
 
 def test_iseven():
     assert not paypay.iseven(1)
@@ -7,3 +10,22 @@ def test_iseven():
     assert paypay.iseven(2)
     assert paypay.iseven(200)
     assert paypay.iseven(230132)
+
+
+@pytest.fixture(scope="session")
+def spark_session(request):
+    """ fixture for creating a spark Session
+    Args:
+        request: pytest.FixtureRequest object
+    """
+
+    spark = SparkSession.builder.master('local[*]').appName('local-test').getOrCreate()
+
+    request.addfinalizer(lambda: spark.stop())
+
+    return spark
+
+
+def test_count_customers(spark_session):
+    assert paypay.count_customers('sample.csv', spark_session) == 193
+
